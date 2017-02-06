32581afc964fad3ae9c60c591d3b03ea738b4ddc
Imac-marc <marc.arnoult@hotmail.fr>
2017-02-06 14:56:15 +0100

    modif controller

--- core/Controller/Controller.php
+++ core/Controller/Controller.php
@@ -9,10 +9,8 @@ class Controller
 {
     public function render(Request $request)
     {
-        //extract($request->attributes->all(), EXTR_SKIP);
+        extract($request->attributes->all(), EXTR_SKIP);
         ob_start();
-        var_dump($request->attributes->all());
-        die();
         include sprintf(__DIR__.'/../../web/views/%s.php', $_route);
 
         return new Response(ob_get_clean());
