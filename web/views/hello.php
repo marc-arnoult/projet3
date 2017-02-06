9423144202d6a55b50a043c6c56b9ba93522e62c
Imac-marc <marc.arnoult@hotmail.fr>
2017-02-06 17:01:57 +0100

    modif view hello

--- web/views/hello.php
+++ web/views/hello.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?= $title ?></title>
+        <title><?= isset($title) ? $title : 'my website' ?></title>
     </head>
     <body>
-        Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
+        <p>Bonjour</p>
     </body>
 </html>
