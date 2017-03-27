97e89fd0e6a84e07954fe9835340f908748d6692
Imac-marc <marc.arnoult@hotmail.fr>
2017-03-27 14:39:24 +0200

    optimize css/js/img

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 module.exports = {
     entry: {
@@ -6,7 +9,8 @@ module.exports = {
             './public/js/app.js',
             './public/js/home.js',
             './public/js/comment.js',
-            './public/js/comment_edit.js'
+            './public/js/comment_edit.js',
+            './public/css/app.css'
         ]
     },
     output: {
@@ -15,17 +19,17 @@ module.exports = {
     },
     module: {
         loaders: [
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css']
-            }
+            { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })}
         ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
+        new ExtractTextPlugin('app.min.css'),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { discardComments: {removeAll: true } },
+            canPrint: true
+        }),
+        new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
     ]
 };
