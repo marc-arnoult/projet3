3f529ebe1216063b2aade881f38d0843d7926a89
Imac-marc <marc.arnoult@hotmail.fr>
2017-04-03 12:08:57 +0200

    fix dir tmp/ for twig

--- core/Controller/Controller.php
+++ core/Controller/Controller.php
@@ -50,7 +50,7 @@ class Controller
             __DIR__ .'/../../resources/views/admin/layout'
         ));
         $twig = new Twig_Environment($loader, array(
-            'cache' => '/../../tmp/'
+            'cache' => __DIR__ .'/../../tmp/'
         ));
         $twig->addExtension(new \Twig_Extensions_Extension_Text());
 
